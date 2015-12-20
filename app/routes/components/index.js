4e5720a3b38f4ef8c58098f5516dbe9d3f743eae
Chris Mckenzie <chrismckenzie@notonthehighstreet.com>
2015-12-20 00:48:33 +0000

    Rendered component URL now require .html

--- app/routes/components/index.js
+++ app/routes/components/index.js
@@ -3,21 +3,16 @@ var componentsPath = "../../components";
 var path = require('path');
 
 var buildComponentPath = function (url) {
-  var componentPath = componentsPath + url;
-
-  // Remove the trailing slash that is appened by static asset loader
-  var pathParts = componentPath.split(path.sep);
-  pathParts.pop();
-  componentPath = pathParts.join(path.sep);
+  var componentPath = componentsPath + url.replace('.html', '');
   return componentPath;
 };
 
 module.exports = function(app) {
 
-  app.get(/.*\/$/, function (req, res) {
+  app.get(/.*\.html$/, function (req, res) {
     var locale = res.locals.locale;
 
-    var componentPath = buildComponentPath(req.originalUrl);
+    var componentPath = buildComponentPath(req.path);
     var component = require(componentPath);
 
     component.render(locale, function(renderedComponent){
