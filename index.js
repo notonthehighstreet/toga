d36e8df55b9e560b79092d5197bf4b17851070f4
Chris Mckenzie, Marek Stasikowski and Simon Adcock <chrismckenzie+marekstasikowski+simonadcock@notonthehighstreet.com>
2016-01-13 11:19:41 +0000

    refactor routes

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 require('babel-core/register');
 const express = require('express');
 const app = express();
-
 const i18n = require('i18n');
-app.use(i18n.middleware);
-app.use(require('./app/routes'));
+const debug = require('debug')('toga');
 
+app.use(i18n.middleware);
+app.use(require('./lib/routes'));
 app.set('views', './public');
 
 const server = app.listen(8080, "0.0.0.0", () => {
   const host = server.address().address;
   const port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  debug('Example app listening at http://%s:%s', host, port);
 });
