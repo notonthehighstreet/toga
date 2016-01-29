0deea54c556559381d39ff3ae3d717717745cab4
Geza Kiss and Simon Adcock <gezakiss+simonadcock@notonthehighstreet.com>
2016-01-29 17:16:26 +0000

    First full component render under 700ms in browser :)

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const app = require('./lib/app');
 const debug = require('debug')('toga');
 const preStartCache = require('./lib/preStartCache');
 
-preStartCache.exec().then(startServer, (err) => console.log('shit', err));
-// @TOdo might be a good idea to not startthe server (run the callback) if an error occour in the cache
-
+preStartCache.exec().then(startServer, (err) => debug(err));
 function startServer() {
   const server = app.listen(8080, '0.0.0.0', () => {
     const host = server.address().address;
