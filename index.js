fe5cbdeaeb80a108c8e99a74e8d6f111aec2bf43
Simon Adcock and Geza Kiss and Marek Stasikowski <simonadcock+gezakiss+marekstasikowski@notonthehighstreet.com>
2016-02-02 16:25:09 +0000

    memcache and redis cacheing

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ require('babel-core/register');
 const app = require('./lib/app');
 const debug = require('debug')('toga');
 const preStartCache = require('./lib/preStartCache');
-
-preStartCache.exec().then(startServer, (err) => debug(err));
 function startServer() {
   const server = app.listen(8080, '0.0.0.0', () => {
     const host = server.address().address;
@@ -12,3 +10,5 @@ function startServer() {
     debug('Example app listening at http://%s:%s', host, port);
   });
 }
+
+preStartCache.exec().then(startServer, (err) => debug(err));
