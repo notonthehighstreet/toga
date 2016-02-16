c73f5fa53c4c8fc63e3ce55cabd16be60319429c
Marek Stasikowski <marekstasikowski@notonthehighstreet.com>
2016-02-16 10:43:32 +0000

    correct path to package.json

--- config/appConfig.js
+++ config/appConfig.js
@@ -2,7 +2,7 @@ const semver = require('semver');
 const applicationConfig = require('./application.json');
 const devConfig = require('./dev.json');
 const mergedConfig = Object.assign({
-  apiVersion: semver.major(require('../../package.json').version)
+  apiVersion: semver.major(require('../package.json').version)
 }, devConfig, applicationConfig);
 
 module.exports = Object.freeze(mergedConfig);
