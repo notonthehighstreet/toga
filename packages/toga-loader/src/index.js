afef0d03a7595170e4536810524b2e2618aa4ac6
Geza Kiss <gezakiss@notonthehighstreet.com>
2016-03-01 15:15:50 +0000

    solving linking bug

---
+++ packages/toga-loader/src/index.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+const loaderUtils = require('loader-utils');
+const bootstrapper = fs.readFileSync(path.join(__dirname, 'bootstrapper.js'));
+const getComponentName = (componentPath) => {
+  const componentPathSegments = componentPath.split('/');
+
+  return componentPathSegments.slice(-2, -1)[0];
+};
+const getEntryComponentName = (entryComponentPath) => {
+  const entryComponentPathSegments = entryComponentPath.split('/');
+
+  return entryComponentPathSegments.slice(2, 3)[0];
+};
+
+module.exports = function(source) {
+  let componentName;
+  let entryComponentName;
+  let isComponentNested;
+  let togaComponentSource = source;
+
+  this.value = togaComponentSource;
+  this.cacheable && this.cacheable();
+  try {
+    componentName = getComponentName(loaderUtils.getRemainingRequest(this));
+    entryComponentName = getEntryComponentName(this.options.entry.components[0]);
+  }
+  catch (e) {
+    return source;
+  }
+  isComponentNested = entryComponentName !== componentName;
+  if (typeof source === 'string' && !isComponentNested) {
+    try {
+      togaComponentSource = `
+        ${source.replace('module.exports', 'let togaComponentSource')}
+        let togaComponentName=\"${entryComponentName}\";
+        ${bootstrapper.toString()}
+      `;
+    }
+    catch (e) {
+      return source;
+    }
+  }
+  this.value = togaComponentSource;
+
+  return togaComponentSource;
+};
