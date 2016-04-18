1e74702f7c0fb085ae74b740ae707555b51d0646
Geza Kiss <gezakiss@notonthehighstreet.com>
2016-04-18 11:05:51 +0100

    [#117609357] adding pre warm cache to breadboard bootstrap

--- script/start/index.js
+++ script/start/index.js
@@ -4,12 +4,10 @@ require('babel-core/register');
 const argv = require('yargs')
   .default('dev', false)
   .argv;
-const preCacheComponentBundle = require('./preCacheComponentBundle');
+
 const startAppServer = require('./startAppServer');
 
-preCacheComponentBundle().then(() => {
-  if (argv.dev) {
-    require('./startDevSyncServer')();
-  }
-  return startAppServer();
-});
+if (argv.dev) {
+  require('./startDevSyncServer')();
+}
+return startAppServer();
