75636be1b18bd50c1f0989c3ba93fe41a14b0621
rogerfranklin2 <rogerfranklin2@users.noreply.github.com>
2016-07-01 10:46:15 +0100

    Updating copyright (#116)
    * Updating copyright

--- components/copyright/index.js
+++ components/copyright/index.js
@@ -1,34 +1,9 @@
 import React from 'react';
-import i18n from './i18n.json';
 
 import './styles.scss';
 
-module.exports = class Copyright extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      'BOOM': false
-    };
-    this.onMouseOver = this.onMouseOver.bind(this);
-  }
-  onMouseOver(boom) {
-    this.setState({
-      'BOOM': boom
-    });
-  }
-  render() {
-    const { locale, one } = this.props;
-    const { BOOM } = this.state;
-    const t = (key) => i18n[locale] && i18n[locale][key] || key;
-    return (
-      <div
-        className={`toga-copyright${BOOM ? ' highlighted' : ''}`}
-        onMouseOver={() => this.onMouseOver(true)}
-        onMouseOut={() => this.onMouseOver(false)}
-      >
-        &copy; 2016 - {t('THANKS')} {one}
-      </div>
-    );
-  }
-};
+module.exports = () =>
+  <div className="toga-copyright">
+    Copyright © 2006–2016 <span className="toga-copyright__company-name">Notonthehighstreet Enterprises Limited</span>
+  </div>;
 
--- components/copyright/spec/index.spec.js
+++ components/copyright/spec/index.spec.js
@@ -1,6 +1,6 @@
-const TestUtils = require('react-addons-test-utils');
-const React = require('react');
-const expect = require('chai').expect;
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
 
 describe('copyright component', () => {
   let Copyright;
@@ -9,31 +9,13 @@ describe('copyright component', () => {
 
   beforeEach(() => {
     Copyright = require('../');
-    renderedComponent = TestUtils.renderIntoDocument(<Copyright/>);
-    componentDOM = TestUtils.findRenderedDOMComponentWithClass(renderedComponent, 'toga-copyright');
+    renderedComponent = shallow(<Copyright/>);
+    componentDOM = renderedComponent.find('.toga-copyright');
   });
 
   describe('initial state', () => {
     it('renders with a copyright class', () => {
-      expect(componentDOM.className).to.equal('toga-copyright');
-    });
-  });
-  describe('hover state', () => {
-    beforeEach(() => {
-      TestUtils.Simulate.mouseOver(componentDOM);
-    });
-
-    it('renders with a highlighted class', () => {
-      expect(componentDOM.className).to.equal('toga-copyright highlighted');
-    });
-
-    describe('hover state is removed', () => {
-      beforeEach(() => {
-        TestUtils.Simulate.mouseOut(componentDOM);
-      });
-      it('renders without a highlighted class', () => {
-        expect(componentDOM.className).to.equal('toga-copyright');
-      });
+      expect(componentDOM.props().className).to.eq('toga-copyright');
     });
   });
 });
