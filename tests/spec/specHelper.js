2bc9b421ab9d66f9c5d4473683bda9c2320af702
Roger Franklin <rogerfranklin@outlook.com>
2016-07-08 16:04:14 +0100

    Adding chai_enzyme and tests for myaccount content

--- tests/spec/specHelper.js
+++ tests/spec/specHelper.js
@@ -2,10 +2,12 @@ const chai = require('chai');
 const sinonChai = require('sinon-chai');
 const chaiAsPromised = require('chai-as-promised');
 const hook = require('node-hook').hook;
+const chaiEnzyme = require('chai-enzyme');
 
 hook('.scss', () => {});
 
 chai.use(chaiAsPromised);
+chai.use(chaiEnzyme());
 require('./setupComponentEnv');
 
 chai.config.includeStack = true;
