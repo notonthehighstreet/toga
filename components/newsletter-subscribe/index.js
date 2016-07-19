26733b114c1f19e4c1ef78738b6fd68d6f6c0046
Peter Mouland <peter.mouland@gmail.com>
2016-07-19 14:05:31 +0100

    fix button height within footer (#157)

--- components/newsletter-subscribe/index.js
+++ components/newsletter-subscribe/index.js
@@ -9,16 +9,13 @@ export default class NewsletterSubscribe extends React.Component {
     const t = (key) => i18n[locale] && i18n[locale][key] || key;
     return (
       <div className="toga-newsletter-subscribe">
-        <p className="newsletter-subscribe__slogan">{t('UNIQUE_INSPIRING')}</p>
-        <p className="newsletter-subscribe__cta">{t('SIGN_UP')}</p>
-        <div className="form-wrapper">
-          <form className="newsletter-subscribe__form" method="post" action="/communication-preference" >
-            <input type="text" className="n-input n-input--subscribe" placeholder={t('ENTER_EMAIL')}/>
-            <button className="n-button n-button--primary n-button--medium newsletter-subscribe__form__submit" type="submit">{t('SUBSCRIBE')}</button>
-          </form>
-        </div>
+        <p className="newsletter-subscribe__slogan hidden--mobile">{t('UNIQUE_INSPIRING')}</p>
+        <p className="newsletter-subscribe__cta hidden--desktop">{t('SIGN_UP')}</p>
+        <form className="newsletter-subscribe__form clearfix" method="post" action="/communication-preference" >
+          <input type="text" className="n-input newsletter-subscribe__input" placeholder={t('ENTER_EMAIL')}/>
+          <button className="n-button n-button--primary n-button--medium newsletter-subscribe__submit" type="submit">{t('SUBSCRIBE')}</button>
+        </form>
       </div>
     );
   }
 }
-
