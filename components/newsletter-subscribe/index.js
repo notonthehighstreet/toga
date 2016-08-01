3a4a3412583a9966a1ddc60dc6598e719fc88494
Peter Mouland <peter.mouland@gmail.com>
2016-08-01 16:58:58 +0100

    a11y

--- components/newsletter-subscribe/index.js
+++ components/newsletter-subscribe/index.js
@@ -8,16 +8,32 @@ import './styles.scss';
 const bem = new BemHelper({ prefix: 'toga-', name: 'newsletter-subscribe'});
 
 export default class NewsletterSubscribe extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      hasEmailText: false
+    };
+    this.onBlur = this.onBlur.bind(this)
+  }
+
+  onBlur(e){
+    this.setState({ hasEmailText : !!e.currentTarget.value })
+  }
+
   render() {
     const { locale } = this.props;
+    const { hasEmailText } = this.state;
     const t = (key) => i18n[locale] && i18n[locale][key] || key;
+    const name = 'newsletter-subscribe-input';
+
     return (
       <div { ...bem() }>
         <p { ...bem('slogan', null, 'hidden--mobile') }>{t('UNIQUE_INSPIRING')}</p>
         <p { ...bem('cta', null, 'hidden--desktop') }>{t('SIGN_UP')}</p>
         <form { ...bem('form') } method="post" action="/communication-preference" >
-          <Label { ...bem('label', null, 'sr-only') } >{t('ENTER_EMAIL')}</Label>
-          <Field { ...bem('input') } type="text" inline placeholder={t('ENTER_EMAIL')}/>
+          <Field { ...bem('input', { 'has-text' : hasEmailText }) } name={name} id={name} type="text" inline onBlur={this.onBlur}/>
+          <Label { ...bem('label') } htmlFor={ name } >{t('ENTER_EMAIL')}</Label>
           <Field { ...bem('submit') } type="submit" inline size="medium" >{t('SUBSCRIBE')}</Field>
         </form>
       </div>
