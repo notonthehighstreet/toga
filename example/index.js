3124e93049e7ce8e0caa086d46fa2e685a41325e
Peter Mouland <peter.mouland@gmail.com>
2016-08-05 13:15:34 +0100

    remove components that were proof of concepts only

--- example/index.js
+++ example/index.js
@@ -4,7 +4,6 @@ var multiple = require('./routes/multiple-components');
 var nested = require('./routes/nested-components');
 var multipleNested = require('./routes/multiple-nested-components');
 var locale = require('./routes/locale-component');
-var productPersonalisation = require('./routes/product-personalisation-component');
 var communication = require('./routes/communication-component');
 
 const server = express();
@@ -14,7 +13,6 @@ server.get('/multiple', multiple);
 server.get('/multiple-nested', multipleNested);
 server.get('/nested', nested);
 server.get('/locale', locale);
-server.get('/product-personalisation', productPersonalisation);
 server.get('/communication', communication);
 
 server.listen('3000', () => {
