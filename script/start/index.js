36c995e97e79de06e892907ead33912f80ce5e1c
Peter Mouland <peter.mouland@gmail.com>
2016-09-08 15:53:03 +0100

    move the newrelic call as high as we can

--- script/start/index.js
+++ script/start/index.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 require('babel-core/register');
 const config = require('../../app/config/index')();
+if (config.newRelicEnabled) {
+  try {
+    require('newrelic');
+  }
+  catch(e) {
+    console.log(e); // eslint-disable-line
+  }
+}
 
 const startAppServer = require('./startAppServer');
-
 if (config.syncServer.enabled) {
   require('./startDevSyncServer')();
 }
--- script/start/startAppServer.js
+++ script/start/startAppServer.js
@@ -1,31 +1,21 @@
 module.exports = function startAppServer() {
-  const bootstrapApp = require('../../index');
   const config = require('../../app/config/index')();
-
-  if (config.newRelicEnabled) {
-    try {
-      require('newrelic');
-    }
-    catch(e) {
-      console.log(e); // eslint-disable-line
-    }
-  }
-
+  const bootstrapApp = require('../../index');
   return bootstrapApp({
     port: config.server.port,
     host: config.server.host
   })
-  .then(({deps: {'/logger': getLogger}, entryResolveValue: server}) => {
-    const host = server.address().address;
-    const port = server.address().port;
-    const logger = getLogger();
+    .then(({deps: {'/logger': getLogger}, entryResolveValue: server}) => {
+      const host = server.address().address;
+      const port = server.address().port;
+      const logger = getLogger();
 
-    logger.info('Toga server listening at http://%s:%s', host, port);
+      logger.info('Toga server listening at http://%s:%s', host, port);
 
-    return server;
-  })
-  .catch((e) => {
-    process.stderr.write(e.stack);
-    process.exit(1);
-  });
+      return server;
+    })
+    .catch((e) => {
+      process.stderr.write(e.stack);
+      process.exit(1);
+    });
 };
