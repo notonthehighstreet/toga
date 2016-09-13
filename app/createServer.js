379e5c1fc5cf47271d2d5f69b0e6276b4ec88ead
Roger Franklin <rogerfranklin@outlook.com>
2016-09-13 09:27:43 +0100

    Returning hash as etag header and listening for If-None-Match on req

--- app/createServer.js
+++ app/createServer.js
@@ -7,13 +7,27 @@ module.exports = (deps) => {
       '/routes/index': getRoutes,
       '/middleware/errorHandler': errorHandler,
       '/middleware/logRequests': logRequests,
+      '/lib/getComponentInfo': getComponentInfo,
+      '/lib/bundler/buildHash': buildHash,
       compression
     } = deps;
     const app = express();
 
+    const componentInfo = getComponentInfo(['vendor']);
+    const hash = buildHash([componentInfo.root])
+
     hook.hook('.scss', () => {});
     app.set('Accept-Encoding', 'gzip');
+    app.set('etag', () => hash);
     app.use(logRequests); //be first to ensure all requests are logged
+    app.use((req, res, next) => {
+      if(req.get('If-None-Match') === hash) {
+        res.sendStatus(304)
+      }
+      else {
+        next()
+      }
+    });
     app.use(compression());
     app.use(domainMiddleware);
     app.use(getRoutes());
