fc4ab269e3a77f8ab05487a2bdcd638886e41499
Peter Mouland <peter.mouland@gmail.com>
2016-09-29 11:54:41 +0100

    optimise

--- tests/e2e/components/svg/index.js
+++ tests/e2e/components/svg/index.js
@@ -1,20 +1,18 @@
 import React, { PropTypes } from 'react';
 import bemHelper from 'react-bem-helper';
 
-const bem = bemHelper({ prefix : 'toga-', name: 'svg' });
+const bem = bemHelper({ prefix: 'toga-', name: 'svg' });
 
-export default class Svg extends React.Component {
+const Svg = ({ markup, className, ...props }) => {
+  const isBase64 = typeof markup === 'string' && markup.indexOf('data') === 0;
+  const classes = bem(null, null, className);
+  return isBase64
+    ? <img src={markup} { ...classes } {...props} />
+    : <div dangerouslySetInnerHTML={{ __html: markup }} { ...classes } {...props} ></div>;
+};
 
-  static propTypes = {
-    markup: PropTypes.string.isRequired
-  };
+Svg.propTypes = {
+  markup: PropTypes.string.isRequired,
+};
 
-  render() {
-    const { markup, className, ...props } = this.props;
-    const isBase64 = typeof markup === 'string' && markup.indexOf('data') === 0;
-    const classes = bem(null, null, className);
-    return isBase64
-      ? <img src={markup} { ...classes } {...props} />
-      : <div dangerouslySetInnerHTML={{ __html: markup }} { ...classes } {...props} ></div>;
-  }
-}
+export default Svg;
