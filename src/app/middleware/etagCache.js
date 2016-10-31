68826c0c762c20891858d979466019f7a39271bf
Roger Franklin <rogerfranklin@outlook.com>
2016-10-31 13:53:55 +0000

    Setting newrelic controller name for etag responses

--- src/app/middleware/etagCache.js
+++ src/app/middleware/etagCache.js
@@ -1,14 +1,20 @@
 module.exports = (deps) => {
   const {
-    '/lib/bundler/buildHash': buildHash
+    '/lib/bundler/buildHash': buildHash,
+    '/config/index': getConfig
   } = deps;
 
-  const hash = buildHash();
+  const config = getConfig();
 
+  const hash = buildHash();
   const returnHash = () => hash;
 
   const etagRequest = (req, res, next) => {
     if(req.get('If-None-Match') === hash) {
+      if(config.newRelicEnabled) {
+        const newRelic = require('newrelic');
+        newRelic.setControllerName('ETAG 304 Not Modified');
+      }
       res.sendStatus(304);
     }
     else {
