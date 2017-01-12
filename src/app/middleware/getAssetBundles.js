bfa3419d27baa271f95556670cb8054f214dca2d
Peter Mouland <peter.mouland@gmail.com>
2017-01-12 17:25:53 +0000

    change bundle to use dist for non-babel use

--- src/app/middleware/getAssetBundles.js
+++ src/app/middleware/getAssetBundles.js
@@ -1,19 +1,20 @@
-const { assetUrl } = require('../lib/utils/assetUrl');
-
 module.exports = (deps) => {
   const {
     '/lib/utils/bundleFilename': bundleFilename,
-    '/config/index': getConfig
+    '/config/index': getConfig,
+    '/lib/getComponentInfo': getComponentInfo
   } = deps;
 
   return function getAssetBundles(req, res) {
     const { components = {} } = getConfig();
+    const allComponents = getComponentInfo();
     const jsonResponse = {};
-    (components.bundles || []).forEach(bundle => {
+    const allBundles = allComponents.concat(components.bundles);
+    allBundles.forEach(bundle => {
       const filename = bundleFilename(bundle.name, { minify: true });
       jsonResponse[bundle.name] = {
-        js: `${assetUrl()}${bundle.name}/${filename}.js`,
-        css: `${assetUrl()}${bundle.name}/${filename}.css`
+        js: `${bundle.name}/${filename}.js`,
+        css: `${bundle.name}/${filename}.css`
       };
     });
     res.json(jsonResponse);
