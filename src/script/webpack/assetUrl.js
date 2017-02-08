d711b379e37883db9d49e9a49350d5d8c53de369
Chris Mckenzie <chrismckenzie@notonthehighstreet.com>
2017-02-08 12:18:25 +0100

    The asset-bundles links to static assets snow use an S3 url instead of the CDN url

--- src/script/webpack/assetUrl.js
+++ src/script/webpack/assetUrl.js
@@ -1,11 +1,26 @@
-const { assets } = require('../../app/config/application');
+const { assets, aws } = require('../../app/config/application');
 
-module.exports.assetUrl = () => {
+function cdn() {
   if (assets.url) {
-    return `//${assets.url}`;
+    return `//${assets.url}/`;
   }
   if (assets.host) {
-    return `//${assets.host}/${assets.prefix}`;
+    return `//${assets.host}/${assets.prefix}/`;
   }
   return '';
+}
+
+function s3() {
+  if (assets.url) {
+    return `//${assets.url}/`;
+  }
+  if (aws.bucket) {
+    return `//${aws.bucket}.s3.amazonaws.com/${assets.prefix}/`;
+  }
+  return '';
+}
+
+module.exports = {
+  cdn,
+  s3
 };
