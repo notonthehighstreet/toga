7eb10ebb5fe27ff8f13279b90e1a94f5c62fbca0
Teofilo Monteiro <teofilo.monteiro@mindera.com>
2017-11-13 14:00:50 +0000

    CAT-1051: Support Post request with data to be render

--- src/app/routes/components.js
+++ src/app/routes/components.js
@@ -2,13 +2,17 @@ module.exports = (deps) => {
   return function createComponentRouter() {
     const {
       'express': express,
+      'body-parser': bodyParser,
       '/middleware/setComponentProps': setComponentProps,
-      '/middleware/getComponentHtml': getComponentHtml
+      '/middleware/RenderController': RenderController,
     } = deps;
     const router = express.Router();
 
     router.use('/:componentName.(raw\.|preview\.)?html', setComponentProps.html);
-    router.get('/:componentName.(raw\.|preview\.)?html', getComponentHtml);
+    router.get('/:componentName.(raw\.|preview\.)?html', (req, res, next) => new RenderController(req, res, next).render());
+
+    router.use(bodyParser.json());
+    router.post('/:componentName.raw.html', (req, res, next) => new RenderController(req, res, next).render(req.body));
 
     // needed for dev
     router.use('/', express.static('dist/components/', { etag: false }));
