d160701b8e3e41e6b6b815677f371b201d57bf1e
José Carlos <jose.silva@mindera.com>
2018-09-03 14:25:53 +0100

    Optionally, pass a new logger instance

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ const devConfig = require('./config.dev');
 const prodConfig = require('./config');
 require('babel-core/register');
 
-module.exports = function bootstrap(initialState) {
-  const breadboardOptions = process.env.TOGA_SOURCE === 'dev'
+module.exports = function bootstrap(initialState, options = {}) {
+  let breadboardOptions = process.env.TOGA_SOURCE === 'dev'
     ? devConfig(initialState)
     : prodConfig(initialState);
+
+  // Replace original logger by a custom one.
+  if (typeof options.logger === 'object' && typeof options.logger.log === 'function') {
+    const getLogger = () => options.logger;
+
+    breadboardOptions = {
+      ...breadboardOptions,
+      substitutes: {
+        ...breadboardOptions.substitutes,
+        '/logger': getLogger
+      },
+    };
+  }
 
   return breadboard(breadboardOptions);
 };
