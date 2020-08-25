c037702e14650d0d3da6c974a309f85d2d74e052
Jonathan Lim <jonlim@notonthehighstreet.com>
2020-08-25 12:50:17 +0100

    Remove newrelic
    Move to package-lock.json

--- src/app/middleware/setComponentProps.js
+++ src/app/middleware/setComponentProps.js
@@ -1,17 +1,7 @@
 module.exports = (deps) => {
   const {
     '/lib/utils/errors': { BadRequestError },
-    '/config/index': getConfig,
-    url
   } = deps;
-  const config = getConfig();
-
-  const setControllerName = (reqUrl) => {
-    if(config.newRelicEnabled) {
-      const newRelic = require('newrelic');
-      newRelic.setControllerName(url.parse(reqUrl).pathname);
-    }
-  };
 
   const encodeJson = (props) => {
     return (props === undefined)
@@ -30,7 +20,6 @@ module.exports = (deps) => {
 
   return {
     html(req, res, next) {
-      setControllerName(req.originalUrl);
       req.componentName = req.params.componentName;
       req.props = parse(req.query.props, next);
       req.assetType = 'html';
--- src/script/start/index.js
+++ src/script/start/index.js
@@ -1,13 +1,5 @@
 #!/usr/bin/env node
 const config = require('../../app/config/index')()();
-if (config.newRelicEnabled) {
-  try {
-    require('newrelic');
-  }
-  catch(e) {
-    console.log(e); // eslint-disable-line
-  }
-}
 
 const startAppServer = require('./startAppServer');
 if (config.syncServer.enabled) {
